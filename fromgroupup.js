26334755a2821334b3c83a8ea56ae6ee43aa7cfa
Michael <michaelleojacob@gmail.com>
2021-06-29 18:52:19 -0700

    Localstorage is all set up and working properly. I can take data from localstorage, assign the data to book cards on the page. You can then delete the individual book card if need be. I think i'm done with this project, really really happy with my solutions to localstorage. Most my functions are clean af, just the last one could use a bit of polishing. A bit being A metric shitton. But eh it works

--- fromgroupup.js
+++ fromgroupup.js
@@ -12,13 +12,11 @@ let myLibrary = [];
 let localStorageIndexCount = localStorage.length;
 
 function doesLocalStorageHaveItems(){
-    console.log("yes");
     if(localStorage.length !== 0){
         for(let i=0; i<localStorage.length; i++){
             myLibrary.push(localStorage[i]);
         }
     }
-    console.log(myLibrary);
 }
 doesLocalStorageHaveItems()
 
@@ -39,6 +37,9 @@ function makeCardforBook(string){
 
 newSubmit.addEventListener("click", (e) => {
     e.preventDefault();
+    if((btitle.value.includes("/")) || (bauthor.value.includes("/")) || (bpages.value.includes("/"))){
+        return displayError.textContent = `the character "/" is not allowed to be used`;
+    }
     if (btitle.value === ""){return displayError.textContent = "Book's title was empty"}
     else if (bauthor.value === ""){return displayError.textContent = "Book's author was empty"}
     else if (bpages.value === ""){return displayError.textContent = "Amount of pages was empty"}
@@ -52,16 +53,30 @@ newSubmit.addEventListener("click", (e) => {
     bpages.value = "";
 
     myLibrary.push(new Book(userTitle, userAuthor, userPages));
-    localStorage.setItem(localStorageIndexCount++, `${userTitle}" by ${userAuthor} \n pages: ${userPages}`)
+    localStorage.setItem(localStorageIndexCount++, `${userTitle}/${userAuthor}/${userPages}`);
     makeCardforBook(myLibrary[myLibrary.length - 1].title);
 })
+
+function localToCardPlusSplitEachElement(){
+    for(let i=0; i<myLibrary.length; i++){
+        if (!myLibrary[i].includes("/")){
+            return
+        }
+        else{
+            myLibrary[i] = myLibrary[i].split("/");
+            makeCardforBook(myLibrary[i][0])
+        }
+    }
+}
+localToCardPlusSplitEachElement()
 
 document.addEventListener("click", function(e){
     if(e.target.className === "box"){
         clickedon = e.target.innerText;
         for(let i=0;i<myLibrary.length;i++){
-            if(myLibrary[i].title === clickedon){
-                const userSelected = myLibrary[i];
+            //logic for items that the user dynamically creates.
+            if((myLibrary[i].title === clickedon) && (undefined !== myLibrary[i].title)){
+                const userSelected = myLibrary[i]
                 wherebookinfogoes.textContent = `"${userSelected.title}" by ${userSelected.author} \n pages: ${userSelected.pages}`;
                 const del = document.createElement("button");
                 del.className = "deleteOption";
@@ -72,6 +87,31 @@ document.addEventListener("click", function(e){
                         for(let i=0;i<myLibrary.length; i++){
                             const itemToDel = myLibrary.indexOf(userSelected);
                             bookcard = document.getElementById(userSelected.title);
+                            bookcard.remove();
+                            del.remove();
+                            wherebookinfogoes.textContent = "";
+                            delete myLibrary[itemToDel];
+                            delete localStorage[itemToDel]
+                            myLibrary = myLibrary.filter(item => item);
+                            return;
+                        }
+                    }
+                })
+            }
+            //logic for items that were created by localstorage
+            if(myLibrary[i][0] === clickedon){
+                let userSelected = myLibrary[i];
+                wherebookinfogoes.textContent = `"${userSelected[0]}" by ${userSelected[1]} \n pages: ${userSelected[2]}`;const del = document.createElement("button");
+                del.className = "deleteOption";
+                del.textContent = "Delete this book";
+                wherebookinfogoes.appendChild(del);
+                del.addEventListener("click", function(e){
+                    if(myLibrary.includes(userSelected)){
+                        for(let i=0;i<myLibrary.length; i++){
+                            console.log(userSelected);
+                            const itemToDel = myLibrary.indexOf(userSelected);
+                            console.log(itemToDel);
+                            bookcard = document.getElementById(userSelected[0]);
                             bookcard.remove();
                             del.remove();
                             wherebookinfogoes.textContent = "";
